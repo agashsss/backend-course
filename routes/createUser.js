61ac2ad6111efc1616539b890620e2d206ebeb51
sakuna <100260664+agashsss@users.noreply.github.com>
2022-05-16 15:44:06 +0600

    Delete routes directory

--- routes/createUser.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-var path = require('path');
-
-const userController = require("../controllers/userController");
-
-router.post('/', userController.create);
-
-module.exports = router;
